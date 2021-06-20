b3aca5e9ee6e6d173f85d778693376d8434bf969
MohamedAly8 <mohamed_alyaly@outlook.com>
2021-06-20 04:43:20 -0400

    Added CSS for Navbar, Button, App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
 import './App.css';
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
+
 
 
 function App() {
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,8 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import { Button } from './Button';
+import './Navbar.css';
+
 
 function Navbar() {
     const [click, setClick] = useState(false);
@@ -17,13 +20,19 @@ function Navbar() {
         }
     };
 
+    useEffect(() => {
+        showButton();
+      }, []);
+
     window.addEventListener('resize', showButton);
+
+
     return (
         <>
             <nav className="navbar">
                 <div className="navbar-container">
-                    <Link to="/" className="navbar-logo">
-                        TRVL <i class="fas fa-ankh"></i>
+                    <Link to="/" className="navbar-logo" onClick={closeMobileMenu} >
+                        TELUS TSBT <i class="fas fa-ankh"></i>
                     </Link>
                     <div className='menu-icon' onClick={handleClick}>
                         <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
